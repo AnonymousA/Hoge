13999ee2c01bb9dd06f6a317216c839fa2b5fe10
村人 <hoge@hoge>
2013-09-13 01:22:23 +0900

    hoge

---
+++ TEST/src/TEST.java
@@ -0,0 +1,11 @@
+public class TEST {
+	int number;
+	TEST(int z) { number = z; }
+	public static void main(String[] args) {
+		TEST a = new TEST(100);
+		TEST b = a;
+		System.out.println(a.number + ":" + b.number);
+		a.number = 0;
+		System.out.println(a.number + ":" + b.number);
+	}
+}
